d4a35d41d438b0fe169f41b149e36828439dd204
Ousmane Diallo <ousmanediallo3213@gmail.com>
2024-07-03 13:19:02 -0400

    Day 5

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useState, useRef } from "react"
+import { motion } from "framer-motion"
+import emailjs from '@emailjs/browser'
+
+import { styles } from "../styles"
+import { EarthCanvas } from './canvas'
+import { SectionWrapper } from '../hoc'
+import { slideIn } from "../utils/motion"
+
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm ] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+  const [loading, setLoading ] = useState(false);
+  const handleChange = (e) => {}
+  const handleSubmit = (e) => {}
   return (
-    <div>Contact</div>
+  <div className=" xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+  <motion.div variants={slideIn('left', "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl"> 
+  <p className={styles.sectionSubText}>Get In Touch</p>
+  <h3 className={styles.sectionHeadText}> Contact.</h3>
+
+  <form 
+  ref={formRef}
+  onSubmit={handleSubmit}
+  className=" mt-12 gap-8 flex flex-col">
+  <label className="flex flex-col">
+  <span className="text-white font-medium mb-4"> Your Name</span>
+  <input type="text" name="name" value={form.name} onChange={handleChange} placeholder="What is your name?"  className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"/>
+  </label>
+
+  <label className="flex flex-col">
+  <span className="text-white font-medium mb-4"> Your Email Address</span>
+  <input type="text" name="email" value={form.email} onChange={handleChange} placeholder="What is your email address?"  className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"/>
+  </label>
+  <label className="flex flex-col">
+  <span className="text-white font-medium mb-4"> Your  message</span>
+  <textarea type="text" rows="7" name="message" value={form.message} onChange={handleChange} placeholder="message..."  className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"/>
+  </label>
+
+  </form>
+  </motion.div>
+  </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "")
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I am <span className='text-[#915eff]'>Ousmane</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am Junior at <br className='sm:block hidden' />
+            I am a Junior at <br className='sm:block hidden' />
             <span className='mb-3 text-[#915eff]'>CUNY New York City College of Technology. </span>
             I am majoring in <span className='text-[#915eff]'>Computer Systems | Software Engineering.</span>
           </p>
